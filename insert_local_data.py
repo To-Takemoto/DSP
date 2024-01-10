eaf0001bd36d882f3ecdb400e251b6dd6cc82ff4
Takemoto-Tomohiro <s2322039@stu.musashino-u.ac.jp>
2024-01-10 11:38:30 +0900

    引数を追加

--- insert_local_data.py
+++ insert_local_data.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title("DSP最終課題")
+sns = st.text_input("SNS")
+Entame = st.text_input("Entame")
+Zyouhou = st.text_input("Zyouhou")
+Sonohoka = st.text_input("Sonohoka")
+Creatibity = st.text_input("Creatibity")
+Sigoto = st.text_input("Sigoto")
+Shopping = st.text_input("Shopping")
+Util = st.text_input("Util")
+Game = st.text_input("Game")
