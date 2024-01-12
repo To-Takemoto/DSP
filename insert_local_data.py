251fc528309e925dfb5e9c9a5272a38672dce2d0
Takemoto-Tomohiro <s2322039@stu.musashino-u.ac.jp>
2024-01-12 23:03:46 +0900

    復旧中…

--- insert_local_data.py
+++ insert_local_data.py
@@ -33,9 +33,9 @@ if st.button("a"):
         date = f"{int(year)}年{int(month)}月{int(date)}日"
         result_dic = dict(zip(type_list, result_list_int))
         result_dic = result_dic | {"date":date}
-
         check_list = ["date"]
         utils.DBHandler(settings.db_path).insert_data("screen_time_table", result_dic, check_list)
+        print("Done!")
     except ValueError:
         st.write("年月が空欄かも…")
         
